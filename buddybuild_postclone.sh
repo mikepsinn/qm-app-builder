29fd0b48399f12a150360b970b94091a16b1a3b0
Mike P. Sinn <m@quantimodo.com>
2017-07-28 16:28:19 -0500

    sudo rm -rf app/scripts

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+sudo rm -rf app/scripts
 git submodule sync --recursive
 git submodule update --remote --recursive  # Should get the most recent commit of the branch specified in .gitmodules
 source app/buddybuild_postclone.sh
